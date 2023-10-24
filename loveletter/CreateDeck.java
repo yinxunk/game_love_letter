0116c078b61a24b5c76d7c6ae557a0fe39db0d1e
Yin Xun <kho.yinxun@gmail.com>
2023-10-24 17:30:59 +0200

    changed the way of how deck is created

--- loveletter/CreateDeck.java
+++ loveletter/CreateDeck.java
@@ -6,22 +6,36 @@ import java.util.Random;
 public class CreateDeck {
     public static ArrayList<Cards> createDeck(){
     ArrayList<Cards> Characters = new ArrayList<>(16);
-    Characters.add(new Cards(8, "Princess"));
-    Characters.add(new Cards(7, "Countess"));
-    Characters.add(new Cards(6, "King"));
-    Characters.add(new Cards(5, "Prince"));
-    Characters.add(new Cards(5, "Prince"));
-    Characters.add(new Cards(4, "Handmaid"));
-    Characters.add(new Cards(4, "Handmaid"));
-    Characters.add(new Cards(3, "Baron"));
-    Characters.add(new Cards(3, "Baron2"));
-    Characters.add(new Cards(2, "Priest"));
-    Characters.add(new Cards(2, "Priest"));
-    Characters.add(new Cards(1, "Guard"));
-    Characters.add(new Cards(1, "Guard"));
-    Characters.add(new Cards(1, "Guard"));
-    Characters.add(new Cards(1, "Guard"));
-    Characters.add(new Cards(1, "Guard"));
+    Cards princess = new Cards(8, "Princess");
+    Cards countess = new Cards(7, "Countess");
+    Cards king = new Cards(6, "King");
+    Cards prince =  new Cards(5, "Prince");
+    
+    Cards handmaid = new Cards(4, "Handmaid");
+    
+    Cards baron = new Cards(3, "Baron");
+    
+    Cards priest = new Cards(2, "Priest");
+    
+    Cards guard = new Cards(1, "Guard");
+    
+
+    Characters.add(princess);
+    Characters.add(countess);
+    Characters.add(prince);
+    Characters.add(prince);
+    Characters.add(handmaid);
+    Characters.add(handmaid);
+    Characters.add(baron);
+    Characters.add(baron);
+    Characters.add(king);
+    Characters.add(priest);
+    Characters.add(priest);
+    Characters.add(guard);
+    Characters.add(guard);
+    Characters.add(guard);
+    Characters.add(guard);
+    Characters.add(guard);
     
     int i  = new Random().nextInt(Characters.size());
     Cards setaside = Characters.remove(i);
