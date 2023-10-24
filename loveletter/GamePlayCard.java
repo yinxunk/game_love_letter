ec51122a692393f51257a37215330bbf8f84b05a
Yin Xun <kho.yinxun@gmail.com>
2023-10-24 17:29:57 +0200

    fixed datatype

--- loveletter/GamePlayCard.java
+++ loveletter/GamePlayCard.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GamePlayCard{
-    public static String playCard(Players player, ArrayList<Cards> characters){
+    public static void playCard(Players player, ArrayList<Cards> characters){
         Scanner scannerforgame = new Scanner(System.in);
         System.out.println("To play a card, please type \\playCard");
         String play = scannerforgame.nextLine();
@@ -14,18 +14,31 @@ public class GamePlayCard{
             GameDrawCard.drawCard(player, characters );
             System.out.println("Only the cards in your hand can be played");
             System.out.println(GameShowHand.showHand(player));
-            Scanner playedcard = new Scanner(System.in);
-            System.out.println("Choose a card to play");
-            String card = playedcard.nextLine();
-            if(player.getHands().contains(card)){
-                player.getHands().remove(card);
-                player.playedCard(card);
-                System.out.println("player played" + player.getPlayedCard());
+            boolean trueorfalse = true;
+            while (trueorfalse == true){
+                Scanner playedcard = new Scanner(System.in);
+                System.out.println("Choose a card to play");
+                String card = playedcard.nextLine();
+                for(int i = 0; i < player.getHands().size(); i++){
+                    if(player.getHands().get(i).getName().equals(card)){ 
+                        player.getHands().remove(0);
+                        player.playedCard(card);
+                        System.out.println("player played" + player.getPlayedCard());
+                        trueorfalse = false;
+                        break;
+                
+                    }
+                    else{
+                        System.out.println("You can't play something you don't have");
+                        
+                    
+                    }
+                }
+                
+                
 
-             }
-            else{
-                System.out.println("You can't play something you don't have");
             }
+            
         }
         else{
             System.out.println("Make sure you spelled correctly");
@@ -34,7 +47,11 @@ public class GamePlayCard{
         
         
         
-        return "End of turn";
+        System.out.println("End of turn");
     }
+    public static void main(String[] args) {
+        
+    }
+    
     
 }
